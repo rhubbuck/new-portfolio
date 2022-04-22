93b1a765d20959fcc2d0d98b1421c96975944b87
Ryan Hubbuck <rhubbuck@gmail.com>
2022-04-22 12:52:19 -0400

    mobile menu

--- new-portfolio/src/Header.js
+++ new-portfolio/src/Header.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css';
 
 import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 const Header = () => {
+  const [ showMenu, setShowMenu ] = useState(false);
 
+  const openMenu = () => {
+    // console.log(isSidebar)
+    setShowMenu(!showMenu);
+  }
 
   return (
     <div className='bg-slate-900 flex justify-between py-6 md:px-32 px-6 fixed w-full z-10 items-center'>
@@ -18,9 +24,21 @@ const Header = () => {
             </ul>
         </div>
         <div className='md:hidden'>
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white" stroke-width="2">
+          <svg onClick={openMenu} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
+          {showMenu &&
+          <div className="mobile-menu fixed right-0 w-full h-full mt-4 background-shade font-jakarta">
+            <div className='bg-white w-3/5 h-full fixed right-0 transition-all duration-200'>
+              <ul className=" text-slate-900 pt-4">
+                <li class="active"><Link to='hero' spy={true} smooth={true} offset={-90} onClick={openMenu} className="block px-2 py-4 underline">Home</Link></li>
+                <li><Link to='about' spy={true} smooth={true} offset={-90} onClick={openMenu} className="block px-2 py-4  transition duration-300 underline">About</Link></li>
+                <li><Link to='work' spy={true} smooth={true} offset={-90} onClick={openMenu} className="block px-2 py-4  transition duration-300 underline">My Work</Link></li>
+                <li><Link to='contact' spy={true} smooth={true} offset={-90} onClick={openMenu} className="block px-2 py-4  transition duration-300 underline">Contact</Link></li>
+              </ul>
+            </div>
+          </div>
+          }
         </div>
     </div>
   )
